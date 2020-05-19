b6478023d2289fa41cde331b98b4801dcf7861af
BMSVieira <brunobmsv@gmail.com>
2020-05-19 22:12:30 +0100

    Update bvselect.js

--- js/bvselect.js
+++ js/bvselect.js
@@ -1,48 +1,48 @@
 (function($) {
-  $.fn.BVSelect = function(parameters) {
+    $.fn.BVSelect = function(parameters) {
 
-      var selectorID = $(this).attr("id");
+        var selectorID = $(this).attr("id");
         var select = $(this);
         var selected = $("option", this);
-        var randomID = Math.floor(Math.random() * (1000 - 0 + 1) ) + 0;
+        var randomID = Math.floor(Math.random() * (1000 - 0 + 1)) + 0;
 
         // ** ---------- INICIO METODOS ----------- **
-        if (typeof parameters == 'string') {  
+        if (typeof parameters == 'string') {
 
-          // ** DESTROY **
-          if (parameters == 'destroy') {
-            this.each(function() {
-              var divselect = $(this).next('.bv_mainselect');
-              if (divselect.length > 0) {
-                divselect.remove();
-                select.css('display', 'block');
-              }
-            });
-          } 
-          // ** UPDATE **  
-          if (parameters == 'update') {
-            this.each(function() {
+            // ** DESTROY **
+            if (parameters == 'destroy') {
+                this.each(function() {
+                    var divselect = $(this).next('.bv_mainselect');
+                    if (divselect.length > 0) {
+                        divselect.remove();
+                        select.css('display', 'block');
+                    }
+                });
+            }
+            // ** UPDATE **  
+            if (parameters == 'update') {
+                this.each(function() {
 
-        			var maindiv = $(this).next(".bv_mainselect");
-        			var width_existente = $(this).next(".bv_mainselect").width() + "px"; 
-        	    var id_generated = $(this).next(".bv_mainselect").children().attr("id");
-        	    id_generated = id_generated.replace("main_", "ul_");
+                    var maindiv = $(this).next(".bv_mainselect");
+                    var width_existente = $(this).next(".bv_mainselect").width() + "px";
+                    var id_generated = $(this).next(".bv_mainselect").children().attr("id");
+                    id_generated = id_generated.replace("main_", "ul_");
 
-        			var searchbox = $(this).next(".bv_mainselect").data('search');
+                    var searchbox = $(this).next(".bv_mainselect").data('search');
 
-	         	  maindiv.remove();
+                    maindiv.remove();
 
-	          	// Configurações do Element existente
-	            var parameters = {
-				    	width: width_existente,
-				    	searchbox: searchbox
-				}
+                    // Configurações do Element existente
+                    var parameters = {
+                        width: width_existente,
+                        searchbox: searchbox
+                    }
 
-	    		// Chama a função para começar a fazer o update
-	   		    CriarBVSelect(select, parameters);
-            });
-          } 
-          return this;
+                    // Chama a função para começar a fazer o update
+                    CriarBVSelect(select, parameters);
+                });
+            }
+            return this;
         }
         // ** ---------- FIM METODOS ----------- **
 
@@ -52,68 +52,76 @@
         // Chama a função para começar a fazer o update
         CriarBVSelect(select, parameters);
 
-        function CriarBVSelect(options, config)
-        {
-       		  // ** CONSTROI ELEMENTOS ** 
-            options.after($('<div id="bv_'+randomID+'" data-search="'+config.searchbox+'" style="width:'+config.width+';"></div>').addClass('bv_mainselect ').addClass(options.attr('class') || '').addClass(options.attr('disabled') ? 'disabled' : '').attr('tabindex', options.attr('disabled') ? null : '0'));
-            $("#bv_"+randomID).append('<div id="main_'+randomID+'" class="bv_atual bv_background"></div><ul id="ul_'+randomID+'" class="bv_ul_inner bv_background"></ul>');
+        function CriarBVSelect(options, config) {
+            // ** CONSTROI ELEMENTOS ** 
+            options.after($('<div id="bv_' + randomID + '" data-search="' + config.searchbox + '" style="width:' + config.width + ';"></div>').addClass('bv_mainselect ').addClass(options.attr('class') || '').addClass(options.attr('disabled') ? 'disabled' : '').attr('tabindex', options.attr('disabled') ? null : '0'));
+            $("#bv_" + randomID).append('<div id="main_' + randomID + '" class="bv_atual bv_background"></div><ul id="ul_' + randomID + '" class="bv_ul_inner bv_background"></ul>');
 
-            if(config.searchbox == true){ $("#ul_"+randomID).prepend('<li class="nofocus"><div class="innerinput"><input placeholder="Search..." class="bv_input" id="input_'+randomID+'" type="text"></div</li>'); }
+            if (config.searchbox == true) {
+                $("#ul_" + randomID).prepend('<li class="nofocus"><div class="innerinput"><input placeholder="Search..." class="bv_input" id="input_' + randomID + '" type="text"></div</li>');
+            }
 
-	          var select_width = $("#main_"+randomID).width();
-	          $( "#ul_"+randomID).css("width", select_width+20+"px");
+            var select_width = $("#main_" + randomID).width();
+            $("#ul_" + randomID).css("width", select_width + 20 + "px");
 
             var selected_option = options.find("option:selected").text();
-            $("#main_"+randomID).html(selected_option+"<i id='arrow_"+randomID+"' class='arrows_bv arrow down'></i>");
-            
-            options.find("option").each(function(index,element) 
-            { 
-                if(element.disabled == true) { var is_disabled = "bv_disabled"} else { is_disabled = ""}
-                if($(this).data('separator') == true) { var is_separator = "bv_separator"} else { is_separator = ""}
-                $( "#ul_"+randomID).append("<li class='"+ is_disabled  +" "+ is_separator  +"'  >"+$(this).text()+"</li>");
+            $("#main_" + randomID).html(selected_option + "<i id='arrow_" + randomID + "' class='arrows_bv arrow down'></i>");
+
+            options.find("option").each(function(index, element) {
+                if (element.disabled == true) {
+                    var is_disabled = "bv_disabled"
+                } else {
+                    is_disabled = ""
+                }
+                if ($(this).data('separator') == true) {
+                    var is_separator = "bv_separator"
+                } else {
+                    is_separator = ""
+                }
+                $("#ul_" + randomID).append("<li class='" + is_disabled + " " + is_separator + "'  >" + $(this).text() + "</li>");
             });
 
             // ** ESCONDE OPCOES ** 
-            $("#main_"+randomID).click(function () {
-	               	$(".bv_ul_inner").hide();
-	                $("#ul_"+randomID).slideDown("fast");
-	                $(".arrows_bv").removeClass("up").addClass("down");
-	                $("#arrow_"+randomID).removeClass("down").addClass("up");
+            $("#main_" + randomID).click(function() {
+                $(".bv_ul_inner").hide();
+                $("#ul_" + randomID).slideDown("fast");
+                $(".arrows_bv").removeClass("up").addClass("down");
+                $("#arrow_" + randomID).removeClass("down").addClass("up");
             });
 
             // ** MUDA OPCAO ** 
-            $("#ul_"+randomID).children().click(function () {
-              	if($(this).hasClass("nofocus") == false)
-              	{
-	                var index = $(this).index();
-	                if($(this).hasClass("bv_disabled"))
-	                {} else {
+            $("#ul_" + randomID).children().click(function() {
+                if ($(this).hasClass("nofocus") == false) {
+                    var index = $(this).index();
+                    if ($(this).hasClass("bv_disabled")) {} else {
 
-	                  $("#main_"+randomID).html($(this).text()+" <i id='arrow_"+randomID+"' class='arrows_bv arrow down'></i>");
-	                  $("#"+selectorID).prop("selectedIndex", index).trigger("change");
-	                  $("#ul_"+randomID).slideUp("fast");
-                    $("#input_"+randomID).val("").keyup();
-	                }
-              	}
+                        $("#main_" + randomID).html($(this).text() + " <i id='arrow_" + randomID + "' class='arrows_bv arrow down'></i>");
+                        $("#" + selectorID).prop("selectedIndex", index).trigger("change");
+                        $("#ul_" + randomID).slideUp("fast");
+                        $("#input_" + randomID).val("").keyup();
+                    }
+                }
             });
 
-	        // ** PROCURA POR RESULTADOS **
-    			$("#input_"+randomID).on("keyup", function() {
-    					var value = this.value.toLowerCase().trim();
-    					$("#ul_"+randomID+" li").show().filter(function() {
-    		              	if($(this).hasClass("nofocus") == false){ return $(this).text().toLowerCase().trim().indexOf(value) == -1;}
-    					}).hide();
-    			});
+            // ** PROCURA POR RESULTADOS **
+            $("#input_" + randomID).on("keyup", function() {
+                var value = this.value.toLowerCase().trim();
+                $("#ul_" + randomID + " li").show().filter(function() {
+                    if ($(this).hasClass("nofocus") == false) {
+                        return $(this).text().toLowerCase().trim().indexOf(value) == -1;
+                    }
+                }).hide();
+            });
 
-          // ** ESCONDE QUANDO CLICA FORA ** 
-          $(document).on('click.bv_mainselect', function(event) {
-              if ($(event.target).closest('.bv_mainselect').length === 0) {
-              	$("#ul_"+randomID).hide();
-              	$("#arrow_"+randomID).removeClass("up").addClass("down");
-                $(".bv_input").val("").keyup();
-              }
-          });
+            // ** ESCONDE QUANDO CLICA FORA ** 
+            $(document).on('click.bv_mainselect', function(event) {
+                if ($(event.target).closest('.bv_mainselect').length === 0) {
+                    $("#ul_" + randomID).hide();
+                    $("#arrow_" + randomID).removeClass("up").addClass("down");
+                    $(".bv_input").val("").keyup();
+                }
+            });
         }
-    return this;
-  };
+        return this;
+    };
 }(jQuery));
