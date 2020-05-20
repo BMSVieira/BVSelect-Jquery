80a1f6ddf378fd0f4e3a56b4987675796da508c8
BMSVieira <brunobmsv@gmail.com>
2020-05-20 11:42:40 +0100

    Major Update to bvselect.js
    Updated Structure

--- js/bvselect.js
+++ js/bvselect.js
@@ -1,72 +1,12 @@
 (function($) {
     $.fn.BVSelect = function(parameters) {
 
-        var selectorID = $(this).attr("id");
-        var select = $(this);
-        var selected = $("option", this);
-        var randomID = Math.floor(Math.random() * (1000 - 0 + 1)) + 0;
+        var selectorID = $(this).attr("id"); // SELECTOR ID
+        var select = $(this); // NATIVE SELECT OBJECT
+        var randomID = Math.floor(Math.random() * (9999 - 0 + 1)) + 0; // RANDOM ID GENERATED
 
-        // ** ---------- INICIO METODOS ----------- **
-        if (typeof parameters == 'string') {
-
-            // ** DESTRUIR **
-            if (parameters == 'destroy') {
-                this.each(function() {
-                    var divselect = $(this).next('.bv_mainselect');
-                    if (divselect.length > 0) {
-                        divselect.remove();
-                        select.css('display', 'block');
-                    }
-                });
-            }
-            // ** ATUALIZAR **  
-            if (parameters == 'update') {
-                this.each(function() {
-
-                    var maindiv = $(this).next(".bv_mainselect");
-                    var width_existente = $(this).next(".bv_mainselect").width() + "px";
-                    var id_generated = $(this).next(".bv_mainselect").children().attr("id");
-                    id_generated = id_generated.replace("main_", "ul_");
-
-                    var searchbox = $(this).next(".bv_mainselect").data('search');
-
-                    maindiv.remove();
-
-                    // Configurações do Element existente
-                    var parameters = {
-                        width: width_existente,
-                        searchbox: searchbox
-                    }
-
-                    // Chama a função para começar a fazer o update
-                    CriarBVSelect(select, parameters);
-                });
-            }
-            return this;
-        }
-        // ** ---------- FIM METODOS ----------- **
-
-        // Esconde o select nativo 
-        $(this).hide();
-
-        // Chama a função para construir os elementos
-        CriarBVSelect(select, parameters);
-
-        function CriarBVSelect(options, config) {
-            // ** CONSTROI ELEMENTOS ** 
-            options.after($('<div id="bv_' + randomID + '" data-search="' + config.searchbox + '" style="width:' + config.width + ';"></div>').addClass('bv_mainselect ').addClass(options.attr('class') || '').addClass(options.attr('disabled') ? 'disabled' : '').attr('tabindex', options.attr('disabled') ? null : '0'));
-            $("#bv_" + randomID).append('<div id="main_' + randomID + '" class="bv_atual bv_background"></div><ul id="ul_' + randomID + '" class="bv_ul_inner bv_background"></ul>');
-
-            if (config.searchbox == true) {
-                $("#ul_" + randomID).prepend('<li class="nofocus"><div class="innerinput"><input placeholder="Search..." class="bv_input" id="input_' + randomID + '" type="text"></div</li>');
-            }
-
-            var select_width = $("#main_" + randomID).width();
-            $("#ul_" + randomID).css("width", select_width + 20 + "px");
-
-            var selected_option = options.find("option:selected").text();
-            $("#main_" + randomID).html(selected_option + "<i id='arrow_" + randomID + "' class='arrows_bv arrow down'></i>");
-
+        // SETUP LIST
+        function SetListBV(options) {
             options.find("option").each(function(index, element) {
                 if (element.disabled == true) {
                     var is_disabled = "bv_disabled"
@@ -81,20 +21,25 @@
                 $("#ul_" + randomID).append("<li class='" + is_disabled + " " + is_separator + "'  >" + $(this).text() + "</li>");
             });
 
-            // ** ESCONDE OPCOES ** 
+            // ** MAIN DIV CLICK ** 
             $("#main_" + randomID).click(function() {
-                $(".bv_ul_inner").hide();
-                $("#ul_" + randomID).slideDown("fast");
-                $(".arrows_bv").removeClass("up").addClass("down");
-                $("#arrow_" + randomID).removeClass("down").addClass("up");
+                // Check if it is open, if yes, close it.
+                if ($("#ul_" + randomID).css('display') == 'block') {
+                    $(".bv_ul_inner").slideUp("fast");
+                    $(".arrows_bv").removeClass("up").addClass("down");
+                } else {
+                    $(".bv_ul_inner").hide();
+                    $("#ul_" + randomID).slideDown("fast");
+                    $(".arrows_bv").removeClass("up").addClass("down");
+                    $("#arrow_" + randomID).removeClass("down").addClass("up");
+                }
             });
 
-            // ** MUDA OPCAO ** 
+            // ** LIST LI CLICK ** 
             $("#ul_" + randomID).children().click(function() {
                 if ($(this).hasClass("nofocus") == false) {
                     var index = $(this).index();
                     if ($(this).hasClass("bv_disabled")) {} else {
-
                         $("#main_" + randomID).html($(this).text() + " <i id='arrow_" + randomID + "' class='arrows_bv arrow down'></i>");
                         $("#" + selectorID).prop("selectedIndex", index).trigger("change");
                         $("#ul_" + randomID).slideUp("fast");
@@ -103,7 +48,7 @@
                 }
             });
 
-            // ** PROCURA POR RESULTADOS **
+            // ** SEARCHBAR  **
             $("#input_" + randomID).on("keyup", function() {
                 var value = this.value.toLowerCase().trim();
                 $("#ul_" + randomID + " li").show().filter(function() {
@@ -113,7 +58,7 @@
                 }).hide();
             });
 
-            // ** ESCONDE QUANDO CLICA FORA ** 
+            // ** HIDE WHEN CLICK OUTSIDE ** 
             $(document).on('click.bv_mainselect', function(event) {
                 if ($(event.target).closest('.bv_mainselect').length === 0) {
                     $("#ul_" + randomID).hide();
@@ -122,6 +67,68 @@
                 }
             });
         }
+
+        // SETUP BASE DIV
+        function SetBaseBV(options, config) {
+
+            options.after($('<div id="bv_' + randomID + '" data-search="' + config.searchbox + '" style="width:' + config.width + ';"></div>').addClass('bv_mainselect ').addClass(options.attr('class') || '').addClass(options.attr('disabled') ? 'disabled' : '').attr('tabindex', options.attr('disabled') ? null : '0'));
+            $("#bv_" + randomID).append('<div id="main_' + randomID + '" class="bv_atual bv_background"></div><ul id="ul_' + randomID + '" class="bv_ul_inner bv_background"></ul>');
+
+            if (config.searchbox == true) {
+                $("#ul_" + randomID).prepend('<li class="nofocus"><div class="innerinput"><input placeholder="Search..." class="bv_input" id="input_' + randomID + '" type="text"></div</li>');
+            }
+
+            var select_width = $("#main_" + randomID).width();
+            $("#ul_" + randomID).css("width", select_width + 20 + "px");
+
+            var selected_option = options.find("option:selected").text();
+            $("#main_" + randomID).html(selected_option + "<i id='arrow_" + randomID + "' class='arrows_bv arrow down'></i>");
+
+            // Append List
+            SetListBV(options);
+        }
+
+        // ** ---------- METHODS ----------- **
+        if (typeof parameters == 'string') {
+
+            // ** DESTROY **
+            if (parameters == 'destroy') {
+                this.each(function() {
+                    var divselect = $(this).next('.bv_mainselect');
+                    if (divselect.length > 0) {
+                        divselect.remove();
+                        select.css('display', 'block');
+                    }
+                });
+            }
+            // ** UPDATE **  
+            if (parameters == 'update') {
+                this.each(function() {
+
+                    var current_id = $(this).next(".bv_mainselect").children().attr("id").match(/\d+/);
+                    var current_searchbox = $(this).next(".bv_mainselect").data('search');
+
+                    randomID = current_id;
+
+                    // Remove all lines without .nofocus class (search input)
+                    $("#ul_" + current_id + " li:not(.nofocus)").remove();
+                    $("#main_" + current_id).off();
+
+                    // Fetches fields and append to main div
+                    SetListBV(select);
+
+                });
+            }
+            return this;
+        }
+        // ** ---------- END METHODS ----------- **
+
+        // Hide Native Select
+        $(this).hide();
+
+        // Setup main div
+        SetBaseBV(select, parameters);
+
         return this;
     };
 }(jQuery));
